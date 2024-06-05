77cf48b2f937140a6ae60ae190291b244f27d118
RoberioAlves <roberiouol@gmail.com>
2024-06-05 10:08:06 -0300

    Aula atualizada e testada.

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+class ProductController {
+    async store(req, res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        });
+
+        try {
+            await schema.validateSync(req.body, { abortEarly: false });
+        } catch (err) {
+            return res.status(400).json({ error: err.errors });
+        }
+        return res.status(201).json({ messsage: "ok" });
+    }
+}
+
+export default new ProductController();
