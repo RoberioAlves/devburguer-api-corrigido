e8e3d2203b177efe103a49dc2804a20d52c65af1
RoberioAlves <roberiouol@gmail.com>
2024-06-16 10:46:41 -0300

    atualizações no codigo.

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from "sequelize";
 import User from "../app/models/User";
 import Product from "../app/models/Products";
 import configDatabase from "../config/database";
+import Category from "../app/models/Category";
 
-const models = [User, Product];
+const models = [User, Product, Category];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,18 @@ import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
 import authMidllewares from "./middlewares/auth";
+import CategoryController from "./app/controllers/CategoryController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+
 routes.use(authMidllewares);
 routes.post("/products", upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index)
+
+routes.post("/categories", CategoryController.store);
+routes.get('/categories', CategoryController.index)
 
 export default routes;
