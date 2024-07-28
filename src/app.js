426f6aed15dd7ee7fc9e0ea1a31c51e462c364c6
RoberioAlves <roberiouol@gmail.com>
2024-07-28 12:29:08 -0300

    alterações no projeto. cors e outros..

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import routes from "./routes";
 import { resolve } from "node:path";
-
+import cors from 'cors';
 import "./database";
 
 class App {
   constructor() {
     this.app = express();
 
+    this.app.use(cors());
     this.middlewares();
     this.routes();
   }
@@ -17,7 +18,13 @@ class App {
       "/product-file",
       express.static(resolve(__dirname, "..", "uploads")),
     );
+
+    this.app.use(
+      "/category-file",
+      express.static(resolve(__dirname, "..", "uploads")),
+    );
   }
+  
 
   routes() {
     this.app.use(routes);
